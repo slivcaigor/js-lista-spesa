212765ac1175bfbe1ef5135a32163bccf043fbe9
slivcaigor <slivcaigor@gmail.com>
2022-11-14 17:24:22 +0100

    Grocery List 'while' cycle

--- js/master.js
+++ js/master.js
@@ -1,10 +1,20 @@
 //Grocery List with 'for' cycle
-let groceries = ['Olio', 'Burro', 'Aceto', 'Sale', 'Zucchero',];
+// let groceries = ['Olio', 'Burro', 'Aceto', 'Sale', 'Zucchero',];
 
+// let list = "";
+// for (let i = 0; i < groceries.length; i++) {
+//   list += groceries[i] + "<br>";
+// };
+
+// document.getElementById("ms_list").innerHTML = list;
+
+//Grocery List with 'while' cycle
+let groceries = ['Olio', 'Burro', 'Aceto', 'Sale', 'Zucchero',];
+let i = 0;
 let list = "";
-for (let i = 0; i < groceries.length; i++) {
+while (i < groceries.length) {
   list += groceries[i] + "<br>";
+  i++
 };
 
 document.getElementById("ms_list").innerHTML = list;
-
